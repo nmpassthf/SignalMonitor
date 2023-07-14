46110949df13c2fb09216e63293a0eaad2d313b5
NormalPassTHFan <nmpassthf@gmail.com>
2023-07-15 01:02:09 +0800

    init empty repo

---
+++ App/Inc/pch.h
@@ -0,0 +1,9 @@
+#ifndef __M_PCH_H__
+#define __M_PCH_H__
+
+#include "config.h"
+
+constexpr auto PROGRAM_NAME = "Signal Monitor";
+constexpr auto VERSION = signalmonitor_VERSION;
+
+#endif /* __M_PCH_H__ */
---
+++ App/Src/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <QWidget>
+#include <QApplication>
+
+#include "pch.h"
+
+
+int main(int argc, char *argv[]) {
+    std::cout << "Starting " << PROGRAM_NAME << std::endl;
+    std::cout << "Current version: " << VERSION << std::endl;
+
+    QApplication app{argc, argv};
+    QWidget w{nullptr};
+
+    w.show();
+
+    return app.exec();
+}
